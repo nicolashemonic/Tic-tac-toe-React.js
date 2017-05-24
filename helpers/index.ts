465cdb77da9f5bace23044376f53e8be1016801f
Nicolas Hémonic <nicolas.hemonic@gmail.com>
2017-05-24 21:21:08 +0200

    Simplify winner options resolution

--- helpers/index.ts
+++ helpers/index.ts
@@ -22,70 +22,38 @@ export function getPlayers() {
     return players;
 }
 
-// Build winning combination
-
-function winnerOptionValuesLine(options, line, difficulty) {
-    var winnerOptionValues = [];
-    for (let i = 1; i <= difficulty; i++) {
-        if (options.find(o => o.value === `X${line}Y${i}`)) {
-            winnerOptionValues.push(`X${line}Y${i}`);
-        }
-    };
-    return winnerOptionValues;
-}
-
-function winnerOptionValuesCol(options, col, difficulty) {
-    var winnerOptionValues = [];
-    for (let i = 1; i <= difficulty; i++) {
-        if (options.find(o => o.value === `X${i}Y${col}`)) {
-            winnerOptionValues.push(`X${i}Y${col}`);
-        }
-    };
-    return winnerOptionValues;
-}
-
-function winnerOptionValuesDiagLeft(options, difficulty) {
-    var winnerOptionValues = [];
-    for (let i = 1; i <= difficulty; i++) {
-        if (options.find(o => o.value === `X${i}Y${i}`)) {
-            winnerOptionValues.push(`X${i}Y${i}`);
-        }
-    };
-    return winnerOptionValues;
-}
-
-function winnerOptionValuesDiagRight(options, difficulty) {
-    var winnerOptionValues = [];
-    var y = difficulty + 1;
-    for (let i = 1; i <= difficulty; i++) {
-        if (options.find(o => o.value === `X${i}Y${y - i}`)) {
-            winnerOptionValues.push(`X${i}Y${y - i}`);
-        }
-    };
-    return winnerOptionValues;
-}
+// Build winner option values
 
 export function getWinnerOptionValues(state) {
-    var winnerOptionValues = [];
+    var linear = [];
+    var diagLeft = [];
+    var diagRight = [];
+    var d = state.difficulty + 1;
 
     state.players.forEach((p) => {
-        let options = state.options.filter(o => o.owner && o.owner.id === p.id);
+        let options: any[] = state.options.filter(o => o.owner && o.owner.id === p.id);
 
-        for (let i = 1; i <= state.difficulty; i++) {
-            if (winnerOptionValues.length < state.difficulty) {
-                winnerOptionValues = winnerOptionValuesLine(options, i, state.difficulty);
-            }
-            if (winnerOptionValues.length < state.difficulty) {
-                winnerOptionValues = winnerOptionValuesCol(options, i, state.difficulty);
-            }
-        }
-        if (winnerOptionValues.length < state.difficulty) {
-            winnerOptionValues = winnerOptionValuesDiagLeft(options, state.difficulty);
-        }
-        if (winnerOptionValues.length < state.difficulty) {
-            winnerOptionValues = winnerOptionValuesDiagRight(options, state.difficulty);
+        for (let dimension = 1; dimension <= state.difficulty; dimension++) {
+            
+            let y = options.filter(o => o.value.indexOf(`Y${dimension}`) > -1);
+            linear = y.length == state.difficulty ? y : linear;
+
+            let x = options.filter(o => o.value.indexOf(`X${dimension}`) > -1);
+            linear = x.length == state.difficulty ? x : linear;
+
+            diagLeft = diagLeft.concat(options.filter(o => o.value.indexOf(`X${dimension}Y${dimension}`) > -1));
+            diagRight = diagRight.concat(options.filter(o => o.value.indexOf(`X${dimension}Y${d - dimension}`) > -1));
         }
     });
-
-    return winnerOptionValues.length === state.difficulty ? winnerOptionValues : [];
+    
+    if (linear.length === state.difficulty) {
+        return linear.map(o => o.value);
+    }
+    if (diagLeft.length === state.difficulty) {
+        return diagLeft.map(o => o.value);
+    }
+    if (diagRight.length === state.difficulty) {
+        return diagRight.map(o => o.value);
+    }
+    return [];
 }
